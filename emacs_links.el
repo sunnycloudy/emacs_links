1ef1ad8af1a0720edd1558bd1937c6e29c551791
sunjun19841229 <remembersunjun1229@gmail.com>
2015-07-22 21:33:22 +0800

    1st

---
+++ emacs_links.el
@@ -0,0 +1,22 @@
+
+  (defun job-search (to-search)
+    (call-process-shell-command
+      (concat
+        "google-chrome \
+'http://search.51job.com/jobsearch/search_result.php?fromJs=1&jobarea=020000%2C00&funtype=0000&industrytype=00&keyword="
+        (s-join "%20%20" (split-string  to-search " ")) "'")))
+
+
+  (defun docker-search (to-search)
+    (call-process-shell-command
+      (concat
+        "google-chrome \
+'https://registry.hub.docker.com/search?q="
+        (s-join "+" (split-string  to-search " ")) "&s=stars'")))
+
+(add-hook 'org-mode-hook
+  (org-add-link-type "job" 'job-search)
+  (org-add-link-type "docker" 'docker-search)
+)
+
+(provide 'url-link)
